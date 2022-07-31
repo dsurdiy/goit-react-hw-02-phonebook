e4ea4231b0b241521f17366b02ce5ff5de1312e2
DimaSurdiy <dsurdiy@ukr.net>
2022-07-31 19:44:14 +0300

    form and contacts list added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      Phonebook App
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleChange = e => {
+    this.setState({ name: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState(({ contacts, name }) => {
+      const contact = {
+        name,
+        id: nanoid(),
+      };
+
+      return {
+        contacts: [contact, ...contacts],
+        name: '',
+      };
+    });
+  };
+
+  render() {
+    const { name, contacts } = this.state;
+
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map(({ name, id }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
